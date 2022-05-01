46ca6cb35c32430814b51e61516d6dc889e3f682
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-05-01 00:26:23 -0500

    initial commit

---
+++ src/main/java/com/project/nibbles/NibblesApplication.java
@@ -0,0 +1,13 @@
+package com.project.nibbles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NibblesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NibblesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/nibbles/NibblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nibbles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NibblesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
